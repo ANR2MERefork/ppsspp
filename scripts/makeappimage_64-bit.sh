e82d1065f9166170a008316ff678a1023c48e100
AdamN <7974720+anr2me@users.noreply.github.com>
2024-06-24 20:43:27 +0700

    Update makeappimage_64-bit.sh

--- scripts/makeappimage_64-bit.sh
+++ scripts/makeappimage_64-bit.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 if [ ! -f appimagetool-x86_64.AppImage ]; then
-    wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+	APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
+	wget -q "$APPIMAGETOOL" -O ./appimagetool-x86_64.AppImage
     chmod +x appimagetool-x86_64.AppImage
 fi
 
@@ -9,6 +10,22 @@ if [ ! -f linuxdeploy-x86_64.AppImage ]; then
     wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
     chmod +x linuxdeploy-x86_64.AppImage
 fi
+
+mkdir ./AppDir/
+mkdir ./AppDir/usr/
+mkdir ./AppDir/usr/bin/
+mkdir ./AppDir/usr/share/
+mkdir ./AppDir/usr/share/applications/
+mkdir ./AppDir/usr/share/icons/
+mkdir ./AppDir/usr/share/icons/hicolor/
+mkdir ./AppDir/usr/share/icons/hicolor/256x256/
+mkdir ./AppDir/usr/share/icons/hicolor/256x256/apps/
+
+cp ./../SDL/PPSSPPSDL.desktop ./AppDir/
+cp ./../SDL/PPSSPPSDL.desktop ./AppDir/usr/share/applications/
+cp ./../build/PPSSPPSDL ./AppDir/usr/bin/
+cp -R ./../build/assets ./AppDir/usr/bin/
+cp ./../icons/hicolor/256x256/apps/ppsspp.png ./AppDir/usr/share/icons/hicolor/256x256/apps/
 
 DESTDIR=AppDir make install
 ./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir=AppDir \
@@ -28,4 +45,5 @@ pushd AppDir
 ln -s usr/share/icons/hicolor/256x256/apps/ppsspp.png
 chmod +x AppRun
 popd
-./appimagetool-x86_64.AppImage --appimage-extract-and-run AppDir
+ARCH=x86_64
+VERSION=$(./AppDir/AppRun --version) ./appimagetool-x86_64.AppImage --appimage-extract-and-run AppDir
