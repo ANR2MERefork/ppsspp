29c8052565cd1f97a0abea473e8387233af8ecfd
AdamN <7974720+anr2me@users.noreply.github.com>
2024-07-12 08:16:49 +0700

    Update makeappimage_64-bit.sh

--- scripts/makeappimage_64-bit.sh
+++ scripts/makeappimage_64-bit.sh
@@ -6,11 +6,6 @@ if [ ! -f appimagetool-x86_64.AppImage ]; then
     chmod +x appimagetool-x86_64.AppImage
 fi
 
-if [ ! -f linuxdeploy-x86_64.AppImage ]; then
-    wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-    chmod +x linuxdeploy-x86_64.AppImage
-fi
-
 mkdir ./AppDir/
 mkdir ./AppDir/usr/
 mkdir ./AppDir/usr/bin/
@@ -28,22 +23,11 @@ cp -R ./../build/assets ./AppDir/usr/bin/
 cp ./../icons/hicolor/256x256/apps/ppsspp.png ./AppDir/usr/share/icons/hicolor/256x256/apps/
 
 DESTDIR=AppDir make install
-./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir=AppDir \
-	--exclude-library="libX*" \
-	--exclude-library="libglib*" \
-	--exclude-library="libgobject*" \
-	--exclude-library="libgdk_pixbuf*" \
-	--exclude-library="libwayland*" \
-	--exclude-library="libgmodule*" \
-	--exclude-library="libgio*" \
-	--exclude-library="libxcb*" \
-	--exclude-library="libxkbcommon*" \
-	--exclude-library="libdb*"
+chmod +x AppDir/AppRun
+./appimagetool-x86_64.AppImage --appimage-extract-and-run -s deploy AppDir/usr/share/applications/*.desktop
 
 rm AppDir/ppsspp.png
 pushd AppDir
 ln -s usr/share/icons/hicolor/256x256/apps/ppsspp.png
-chmod +x AppRun
 popd
-ARCH=x86_64
-VERSION=$(./AppDir/AppRun --version) ./appimagetool-x86_64.AppImage --appimage-extract-and-run AppDir
+ARCH=x86_64 VERSION=$(./AppDir/AppRun --version) ./appimagetool-x86_64.AppImage --appimage-extract-and-run AppDir
