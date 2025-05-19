a3283a9815c1cbb6e166c0a16565cfa97ecdd664
AdamN <7974720+anr2me@users.noreply.github.com>
2025-05-19 14:15:04 +0700

    Try with -S21

--- scripts/makeappimage_64-bit.sh
+++ scripts/makeappimage_64-bit.sh
@@ -56,7 +56,7 @@ echo "Generating AppImage..."
 ./uruntime --appimage-mkdwarfs -f \
 	--set-owner 0 --set-group 0 \
 	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S25 -B8 \
+	--compression zstd:level=22 -S21 -B8 \
 	--header uruntime \
 	-i ./AppDir -o PPSSPP-"$VERSION"-anylinux-"$ARCH".AppImage
 
