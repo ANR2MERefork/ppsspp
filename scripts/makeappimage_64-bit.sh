42df3de68ca2c15387e494961291706c1c7131ec
AdamN <7974720+anr2me@users.noreply.github.com>
2025-05-21 17:05:07 +0700

    try with compression level 19

--- scripts/makeappimage_64-bit.sh
+++ scripts/makeappimage_64-bit.sh
@@ -56,7 +56,7 @@ echo "Generating AppImage..."
 ./uruntime --appimage-mkdwarfs -f \
 	--set-owner 0 --set-group 0 \
 	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S21 -B8 \
+	--compression zstd:level=19 -S21 -B8 \
  	--memory-limit=512M \
   	--num-workers=1 \
    	--num-scanner-workers=$(nproc) \
