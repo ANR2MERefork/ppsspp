391d44d9c8571602e0c0486ea20fe2bb5925a244
AdamN <7974720+anr2me@users.noreply.github.com>
2025-06-14 19:50:33 +0700

    one last time

--- b-ios.sh
+++ b-ios.sh
@@ -25,7 +25,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 # TODO: Generate a self-signed certificate (but probably not a good idea to generate a different cert all the time). Example at https://stackoverflow.com/questions/27474751/how-can-i-codesign-an-app-without-being-in-the-mac-developer-program/53562496#53562496
 
 # Should we run this script first before building?
-pipx install Pillow
+python3 -m pip install --upgrade Pillow
 python3 ../ios/generate_icons.py
 
 # There are 2 ways to build PPSSPP, using make or xcodebuild
