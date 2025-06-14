577fd63d95efd8b7becb8b724f0c9bf5508f1a2e
AdamN <7974720+anr2me@users.noreply.github.com>
2025-06-14 15:33:49 +0700

    Done

--- b-ios.sh
+++ b-ios.sh
@@ -4,9 +4,10 @@ set -ex
 
 # Assuming we're at the ppsspp (repo's root) directory
 mkdir -p build # For the final IPA & DEB file
-#rm -rf build-ios
+#rm -rf build-ios # Should we started from scratch instead of continuing from cache?
 mkdir -p build-ios
 cd build-ios
+rm -rf PPSSPP.app # There seems to be an existing symlink, may be from ccache? We don't want to include old stuff that might be removed to be included in the final IPA file.
 # It seems xcodebuild is looking for "git-version.cpp" file inside "build-ios" directory instead of at repo's root dir.
 echo "const char *PPSSPP_GIT_VERSION = \"$(git describe --always --tags)\";" > git-version.cpp
 echo "#define PPSSPP_GIT_VERSION_NO_UPDATE 1" >> git-version.cpp
@@ -23,18 +24,32 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 </plist>' > exportOptions.plist
 # TODO: Generate a self-signed certificate (but probably not a good idea to generate a different cert all the time). Example at https://stackoverflow.com/questions/27474751/how-can-i-codesign-an-app-without-being-in-the-mac-developer-program/53562496#53562496
 
+# Should we run this script first before building?
+#python3 ../ios/generate_icons.py
+
+# There are 2 ways to build PPSSPP, using make or xcodebuild
+# Generate xcodeproject (only needed when building using xcode, similar to ./b.sh --ios-xcode)
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchains/ios.cmake -GXcode ..
+# Build PPSSPP using xcode
 #xcodebuild clean build -project PPSSPP.xcodeproj CODE_SIGNING_ALLOWED=NO -sdk iphoneos -configuration Release
 xcodebuild -project PPSSPP.xcodeproj -scheme PPSSPP -sdk iphoneos -configuration Release clean build archive -archivePath ./build/PPSSPP.xcarchive CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO #CODE_SIGN_IDENTITY="iPhone Distribution: Your NAME / Company (TeamID)" #PROVISIONING_PROFILE="xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+# Export IPA file from xcarchive (probably only works with signed build)
 #xcodebuild -exportArchive -archivePath ./build/PPSSPP.xcarchive -exportPath ./build -exportOptionsPlist exportOptions.plist
-#ls -R
+# This folder only exist when building with xcodebuild
 if [ -e Release-iphoneos ]; then
-	# It seems there is an existing (from cache?) PPSSPP.app symlink, so we copy the contents instead
+	# It seems there is an existing (from ccache?) PPSSPP.app symlink, so we copy & overwrites the content instead of the symlink, to the same location with what make use.
 	mkdir -p PPSSPP.app
 	cp -Rfa Release-iphoneos/PPSSPP.app/. PPSSPP.app/
 fi
+
+# Build PPSSPP using Makefile (Might have missing stuff like Icons, similar to ./b.sh --ios)
 #cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchains/ios.cmake ..
 #make -j4
+
+# When building with Makefile, we're missing these icon image files (may be need to run generate_icons.py first?). Let's use whatever we have (wrong size tho)
+#cp -a assets/icon_regular_72.png Payload/PPSSPP.app/AppIcon60x60@2x.png
+#cp -a assets/icon_regular_72.png Payload/PPSSPP.app/AppIcon76x76@2x~ipad.png
+
 cp ../ext/vulkan/iOS/Frameworks/libMoltenVK.dylib PPSSPP.app/Frameworks
 ln -s ./ Payload
 #ldid -w -S -IlibMoltenVK -K../../certificate.p12 -Upassword PPSSPP.app/Frameworks/libMoltenVK.dylib
@@ -42,8 +57,6 @@ if [ -e PPSSPP.app/Frameworks/libMoltenVK.dylib ]; then
 	echo "Signing PPSSPP.app/Frameworks/libMoltenVK.dylib ..."
 	ldid -S -IlibMoltenVK PPSSPP.app/Frameworks/libMoltenVK.dylib
 fi
-#cp -a assets/icon_regular_72.png Payload/PPSSPP.app/AppIcon.png
-
 
 echo '<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -72,13 +85,15 @@ fi
 version_number=`echo "$(git describe --tags --match="v*" | sed -e 's@-\([^-]*\)-\([^-]*\)$@-\1-\2@;s@^v@@;s@%@~@g')"`
 echo ${version_number} > PPSSPP.app/Version.txt
 sudo -S chown -R 1004:3 Payload
-echo "Making ipa..."
+
+# Put the xcarchive file in the artifact too, just to examine the contents
+#cp -a build/PPSSPP.xcarchive ../build/
+
+echo "Making ipa ..."
 zip -r9 ../build/PPSSPP_v${version_number}.ipa Payload/PPSSPP.app
-zip -r9 ../build/PPSSPP_v2.ipa "Release-iphoneos"
-cp -a build/PPSSPP.xcarchive ../build/
 echo "IPA DONE :)"
 
-echo "Making deb..."
+echo "Making deb ..."
 package_name="org.ppsspp.ppsspp-dev-latest_v${version_number}_iphoneos-arm"
 mkdir $package_name
 mkdir ${package_name}/DEBIAN
@@ -103,11 +118,9 @@ chmod 0755 ${package_name}/DEBIAN/control
 mkdir ${package_name}/Library
 mkdir ${package_name}/Library/PPSSPPRepoIcons
 if [ -e ../ios/org.ppsspp.ppsspp.png ]; then
-	cp ../ios/org.ppsspp.ppsspp.png ${package_name}/Library/PPSSPPRepoIcons/org.ppsspp.ppsspp-dev-latest.png # 120x120 pixels?
+	cp ../ios/org.ppsspp.ppsspp.png ${package_name}/Library/PPSSPPRepoIcons/org.ppsspp.ppsspp-dev-latest.png # 120x120 pixels? (ie. 60x60@2x)
  	chmod 0755 ${package_name}/Library/PPSSPPRepoIcons/org.ppsspp.ppsspp-dev-latest.png
 fi
-find ../ -name "Preferences"
-find ~/Library -name "Preferences"
 mkdir ${package_name}/Library/PreferenceLoader
 if [ -e ../ios/Preferences ]; then
 	cp -a ../ios/Preferences ${package_name}/Library/PreferenceLoader/
